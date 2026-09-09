9c075c5396421c1ea67b0afe675cd02e3dd464a6
nati24nati25-cyber <nati24nati25@gmail.com>
2026-09-09 23:14:33 +0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -153,7 +153,11 @@ def show_card(entry, images_dir="images"):
         if img_path is None:
             img_path = find_image_by_word(word, images_dir)
         if img_path:
-            st.image(img_path, width=150)
+            if img_path.lower().endswith('.gif'):
+                with open(img_path, 'rb') as f:
+                    st.image(f.read(), width=150)
+            else:
+                st.image(img_path, width=150)
 
         # Дополнительная информация по рубрикам
         if sections:
